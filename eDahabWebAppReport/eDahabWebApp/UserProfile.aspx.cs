e9f4299c9fe8a5ac1b558b71c1be0177f23d37d6
ibrahim mohamed <ibrahim.mohamed738@outlook.com>
2020-05-30 14:01:43 +0300

    Adding UserProfile

---
+++ eDahabWebAppReport/eDahabWebApp/UserProfile.aspx.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace eDahabWebApp
+{
+    public partial class UserProfile : System.Web.UI.Page
+    {
+        BL.CLS_AddUser Register = new BL.CLS_AddUser();
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (Session["Username"] == null)
+                Response.Redirect("Login.aspx");
+
+        }
+
+        protected void btnEditUser_Click(object sender, EventArgs e)
+        {
+            var username = Session["Username"].ToString();
+
+            DataTable dt = Register.CheckUsername(username);
+
+            txtUsername.Text = dt.Rows[0][0].ToString();
+        }
+    }
+}
